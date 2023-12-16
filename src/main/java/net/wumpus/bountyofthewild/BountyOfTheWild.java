425b84125c8edcf18596e5423e9e663cb811f929
Daniel Lalouette <raptyr@hotmail.com>
2023-12-16 15:24:30 +0200

    items structure

--- src/main/java/net/wumpus/bountyofthewild/BountyOfTheWild.java
+++ src/main/java/net/wumpus/bountyofthewild/BountyOfTheWild.java
@@ -1,6 +1,7 @@
 package net.wumpus.bountyofthewild;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.wumpus.bountyofthewild.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -24,6 +26,8 @@ public class BountyOfTheWild
     public BountyOfTheWild()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -40,7 +44,10 @@ public class BountyOfTheWild
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ACORN);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
